c970560072e0060338f7040b7f016ab23d75efef
Chris Pritchard <chris@christopherpritchard.co.uk>
2018-10-21 15:14:30 +0100

    add empty OIDC DB migration script

---
+++ migrations/versions/968144754fed_add_oidc_settings.py
@@ -0,0 +1,24 @@
+"""add_oidc_settings
+
+Revision ID: 968144754fed
+Revises: 31a4ed468b18
+Create Date: 2018-10-21 15:12:40.987069
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '968144754fed'
+down_revision = '31a4ed468b18'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
